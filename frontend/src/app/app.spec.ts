2cadaec803e28d948dd73c2e17127a47d7171277
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2026-04-05 15:48:56 +0300

    fix: frontend test failure

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 
 import { App } from './app';
 import { routes } from './app.routes';
@@ -20,9 +20,13 @@ describe('App', () => {
 
   it('should render welcome page header', async () => {
     const fixture = TestBed.createComponent(App);
+    const router = TestBed.inject(Router);
+
+    await router.navigateByUrl('/');
     fixture.detectChanges();
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to Event Planner');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Welcome to Event Planner');
   });
 });
